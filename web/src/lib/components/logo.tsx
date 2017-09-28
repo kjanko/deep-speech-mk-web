1450bbec560bdbd60d9097fd65cd561ffa61ef98
Kristijan Jankoski <Kristijan.Jankoski@netcetera.com>
2017-09-28 17:19:15 +0200

    Cosmetic changes. Enabled analytics.

--- web/src/lib/components/logo.tsx
+++ web/src/lib/components/logo.tsx
@@ -1,7 +1,7 @@
 import { h, Component } from 'preact';
 
 export default (props) => {
-  return <a class="main-logo" href="/"
+  return <a class="main-logo" href="http://netcetera.com"
     onClick={(evt) =>  {
       evt.preventDefault();
       evt.stopPropagation();
--- web/src/lib/components/pages.tsx
+++ web/src/lib/components/pages.tsx
@@ -332,6 +332,8 @@ export default class Pages extends Component<PagesProps, PagesState> {
     }
 
     return <div id="main" className={className}>
+      <div onClick={this.openInApp} id="install-app">Отвори во апликација
+        <a onClick={this.closeOpenInApp}>X</a></div>
       <header className={(this.state.isMenuVisible || this.state.scrolled ?
                           'active' : '')}>
         <Logo navigate={this.props.navigate}/>
