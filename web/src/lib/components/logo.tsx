44289b7c7c3dfdad63c40f7a22bc0c80c462a5fc
Kristijan Jankoski <Kristijan.Jankoski@netcetera.com>
2017-09-29 13:11:26 +0200

    Cosmetic changes

--- web/src/lib/components/logo.tsx
+++ web/src/lib/components/logo.tsx
@@ -3,9 +3,6 @@ import { h, Component } from 'preact';
 export default (props) => {
   return <a class="main-logo" href="http://netcetera.com"
     onClick={(evt) =>  {
-      evt.preventDefault();
-      evt.stopPropagation();
-      props.navigate('/');
     }}>
     <span class="main-title">Jargon</span><br />
     <span class="secondary-title">netcetera</span><br />
